ddad187a9cd5e8c2afb183345325e627d38c03be
Johnny Ji <johnny.ji@live.ca>
2015-04-28 17:42:11 -0700

    added admin controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,10 @@ Rails.application.routes.draw do
     resources :reviews, only: [:new, :create]
   end
 
+  namespace :admins do
+    resources :users
+  end
+
   resources :users, only: [:new, :create]
   resources :sessions, only: [:new, :create, :destroy]
 end
